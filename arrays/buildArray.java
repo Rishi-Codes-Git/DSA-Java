85f6f5c3057918c422d25227ad48c6215a926ff7
rishicodess <rishinarasimma@gmail.com>
2025-10-21 17:57:39 +0530

    added

---
+++ arrays/buildArray.java
@@ -0,0 +1,13 @@
+// 🔗 Problem: https://leetcode.com/problems/build-array-from-permutation
+// 💡 Approach: Use bruteforce iterating each and every element in array
+// ⏱️ Time: O(n)
+// 💾 Space: O(1)
+class solution {
+    public int[] builddArray(int[] nums) {
+        int[] arr = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            arr[i]=nums[nums[i]];
+        }
+        return arr;
+    }
+}
